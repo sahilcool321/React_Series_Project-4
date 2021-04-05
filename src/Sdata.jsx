81a138754a9cf33074378346724c353ab9b53caf
sahil sharma <sahilkumar4544.com>
2021-04-05 18:03:51 +0530

    add app file

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -1,12 +1,13 @@
 const Sdata = [
   {
+    id: 1,
     sname: "Dark",
-    imgscr:
-      "https://static.wikia.nocookie.net/dark-netflix/images/a/a8/Dark_US_Poster.jpg/revision/latest?cb=20171113104852",
+    imgscr: "https://dark.netflix.io/share/global.png",
     title: "NETFLIX ORIGINAL SERIES",
     links: "https://www.netflix.com/in/title/80100172",
   },
   {
+    id: 2,
     sname: "The Witcher",
     imgscr:
       "https://i2.wp.com/redanianintelligence.com/wp-content/uploads/2019/12/IMG_20191205_160220.jpg?fit=1920%2C1080&ssl=1",
@@ -14,6 +15,7 @@ const Sdata = [
     links: "https://www.netflix.com/in/title/80189685",
   },
   {
+    id: 3,
     sname: "Night Stalker: The Hunt For a Serial Killer",
     imgscr:
       "https://m.media-amazon.com/images/M/MV5BNWM1ODNlZDAtY2U0Yi00YTAyLTgwZWYtNTc2OTFkNDYzZjAxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
@@ -21,6 +23,7 @@ const Sdata = [
     links: "https://www.netflix.com/in/title/81025701",
   },
   {
+    id: 4,
     sname: "Fate: The Winx Saga",
     imgscr:
       "https://s3.amazonaws.com/media.thecrimson.com/photos/2021/01/28/224515_1347979.jpg",
@@ -28,6 +31,7 @@ const Sdata = [
     links: "https://www.netflix.com/in/title/80220679",
   },
   {
+    id: 5,
     sname: "Money Heist",
     imgscr:
       "https://filmdaily.co/wp-content/uploads/2020/06/moneyheist-lede-5-1300x650.jpg",
--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./Cards";
 import "./index.css";
-import Sdata from "./Sdata";
-
-
+import App from "./App";
 
 ReactDOM.render(
   <>
-    <h1 className="heading-style">List of top 5 Netflix Series  </h1>
-    {Sdata.map((val) => {
-      return (
-        <Card
-          imgsrc={val.imgscr}
-          title={val.title}
-          sname={val.sname}
-          link={val.links}
-        />
-      );
-    })}
-    ;
+    <App />
   </>,
   document.getElementById("root")
 );
